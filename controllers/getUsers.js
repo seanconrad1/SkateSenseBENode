afaedc0f4b33b203b79ce4ec4dc63bd054770f44
sean conrad <sean.conrad@deutschinc.com>
2021-04-10 01:20:59 -0400

    pushing up latest

--- controllers/getUsers.js
+++ controllers/getUsers.js
@@ -3,29 +3,29 @@ const Spots = require('../models/spot');
 const Bookmarks = require('../models/bookmark');
 
 module.exports = async function getUsers(req, res, next) {
-  // if (req.isAuth) {
-  const userList = await User.find({})
-    .populate({
-      path: 'spot.owner',
-      model: Spots,
-      populate: [
-        { path: 'image', model: 'Image' },
-        { path: 'location', model: 'Location' },
-      ],
-    })
-    .populate({
-      path: 'bookmark',
-      model: Bookmarks,
-    });
+  if (req.isAuth) {
+    const userList = await User.find({})
+      .populate({
+        path: 'spot.owner',
+        model: Spots,
+        populate: [
+          { path: 'image', model: 'Image' },
+          { path: 'location', model: 'Location' },
+        ],
+      })
+      .populate({
+        path: 'bookmark',
+        model: Bookmarks,
+      });
 
-  if (userList === null) {
-    const errorGetUsers = new Error('Cannot find users');
-    errorGetUsers.code = 404;
-    throw errorGetUsers;
+    if (userList === null) {
+      const errorGetUsers = new Error('Cannot find users');
+      errorGetUsers.code = 404;
+      throw errorGetUsers;
+    }
+
+    res.json(userList);
+  } else {
+    return new Error('Not authenticated');
   }
-
-  res.json(userList);
-  // } else {
-  //   return new Error('Not authenticated');
-  // }
 };
