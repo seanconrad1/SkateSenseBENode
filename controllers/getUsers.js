a9b9af6417e21aa9da6f71f04676f2578ca728df
sean conrad <sean.conrad@deutschinc.com>
2021-04-10 01:09:25 -0400

    pushing up latest

--- controllers/getUsers.js
+++ controllers/getUsers.js
@@ -4,28 +4,28 @@ const Bookmarks = require('../models/bookmark');
 
 module.exports = async function getUsers(req, res, next) {
   // if (req.isAuth) {
-    const userList = await User.find({})
-      .populate({
-        path: 'spot.owner',
-        model: Spots,
-        populate: [
-          { path: 'image', model: 'Image' },
-          { path: 'location', model: 'Location' },
-        ],
-      })
-      .populate({
-        path: 'bookmark',
-        model: Bookmarks,
-      });
+  const userList = await User.find({})
+    .populate({
+      path: 'spot.owner',
+      model: Spots,
+      populate: [
+        { path: 'image', model: 'Image' },
+        { path: 'location', model: 'Location' },
+      ],
+    })
+    .populate({
+      path: 'bookmark',
+      model: Bookmarks,
+    });
 
-    if (userList === null) {
-      const errorGetUsers = new Error('Cannot find users');
-      errorGetUsers.code = 404;
-      throw errorGetUsers;
-    }
-
-    res.json(userList);
-  } else {
-    return new Error('Not authenticated');
+  if (userList === null) {
+    const errorGetUsers = new Error('Cannot find users');
+    errorGetUsers.code = 404;
+    throw errorGetUsers;
   }
-// };
+
+  res.json(userList);
+  // } else {
+  //   return new Error('Not authenticated');
+  // }
+};
